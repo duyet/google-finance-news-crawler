6efa5153367db5e12d8b70292647824c8a444005
Van-Duyet Le <me@duyetdev.com>
2016-12-18 16:22:47 +0700

    Nodejs convert and combine raw JS json to single json of news

---
+++ convert_raw_to_json.js
@@ -0,0 +1,49 @@
+var fs = require('fs');
+var output = [];
+var result = [];
+
+(function readFiles(dirname, onFileContent, onError) {
+    fs.readdir(dirname, function(err, filenames) {
+        if (err) {
+            onError(err);
+            return;
+        }
+
+        filenames.forEach(function(filename) {
+        	output.push(require(dirname + '/' + filename));
+        });
+
+        onFileContent(output);
+    });
+})('./output/', function (output) {
+	for (var i in output) {
+		var row = output[i];
+		var posts = row.clusters;
+
+		// console.log(posts)
+
+		for (var j in posts) {
+			var post = posts[j];
+			if (!post.a) continue;
+			
+			for (var p in post.a) {
+				pp = post.a[p]
+				var item = {
+					title: pp.t,
+					url: pp.u,
+					s: pp.s,
+					sp: pp.sp,
+					date: pp.d,
+					timestamp: pp.tt
+				}
+				result.push(item);
+			}
+		}
+	}
+
+	var json = JSON.stringify(result, null, 4); 
+	fs.writeFile('pandora.json', json, (err) => {
+		if (!err) console.log('Success!')
+		else console.log('Error!');
+	});
+});
